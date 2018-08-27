a827c07d0f1ee81ec631e1285418e091690c5831
Igor Novak <bezengi@gmail.com>
2018-08-27 04:47:05 +0200

    Add color-picker

--- src/control-panel/ControlPanel.js
+++ src/control-panel/ControlPanel.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import ColorPicker from 'rc-color-picker';
+import 'rc-color-picker/assets/index.css';
 import './ControlPanel.css';
 
 class ControlPanel extends Component {
-    format(action) {
-        document.execCommand(action, false, null);
+    format(action, value = null) {
+        document.execCommand(action, false, value);
     }
 
     render() {
@@ -13,6 +15,11 @@ class ControlPanel extends Component {
                     <button className="format-action" type="button" onClick={() => this.format('bold')}><b>B</b></button>
                     <button className="format-action" type="button" onClick={() => this.format('italic')}><i>I</i></button>
                     <button className="format-action" type="button" onClick={() => this.format('underline')}><u>U</u></button>
+                    <ColorPicker
+                        className="format-action"
+                        color={"#369"}
+                        onChange={data => this.format('forecolor', data.color)}
+                    />
                     <button className="format-action" type="button" onClick={() => this.format('outdent')}>&lt;</button>
                     <button className="format-action" type="button" onClick={() => this.format('indent')}>&gt;</button>
                 </div>
