f6df077e8146a35fff4f20f72e9d80290f80e95d
Igor Novak <bezengi@gmail.com>
2018-08-27 05:02:54 +0200

    Add react-contextmenu component

--- src/file-zone/FileZone.js
+++ src/file-zone/FileZone.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
+import { ContextMenu, MenuItem, ContextMenuTrigger } from 'react-contextmenu';
 import './FileZone.css';
 
 class FileZone extends Component {
     constructor(props, context) {
         super(props, context);
         document.execCommand('styleWithCss', false, true);
+        this.state = {
+            word: ''
+        };
+    }
+
+    onContextMenu() {
+        const { focusNode, anchorOffset, focusOffset } = getSelection();
+        const word = focusNode.data.slice(anchorOffset, focusOffset);
+        this.setState({
+            word
+        });
     }
 
     render() {
         return (
             <div id="file-zone">
-                <div id="file" contentEditable={true}>
-
-                </div>
+                <ContextMenuTrigger id="context-menu-trigger">
+                    <div id="file" contentEditable={true}></div>
+                </ContextMenuTrigger>
+                <ContextMenu id="context-menu-trigger" onShow={this.onContextMenu.bind(this)}>
+                    <MenuItem disabled>Synonyms for "{this.state.word}":</MenuItem>
+                    <MenuItem divider />
+                </ContextMenu>
             </div>
         );
     }
