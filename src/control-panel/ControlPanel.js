20eaadc01981cc41854a8bb163ffe336b6f529e7
Igor Novak <bezengi@gmail.com>
2018-08-27 04:32:00 +0200

    Implement text formatting

--- src/control-panel/ControlPanel.js
+++ src/control-panel/ControlPanel.js
@@ -2,13 +2,19 @@ import React, { Component } from 'react';
 import './ControlPanel.css';
 
 class ControlPanel extends Component {
+    format(action) {
+        document.execCommand(action, false, null);
+    }
+
     render() {
         return (
             <div id="control-panel">
                 <div id="format-actions">
-                    <button className="format-action" type="button"><b>B</b></button>
-                    <button className="format-action" type="button"><i>I</i></button>
-                    <button className="format-action" type="button"><u>U</u></button>
+                    <button className="format-action" type="button" onClick={() => this.format('bold')}><b>B</b></button>
+                    <button className="format-action" type="button" onClick={() => this.format('italic')}><i>I</i></button>
+                    <button className="format-action" type="button" onClick={() => this.format('underline')}><u>U</u></button>
+                    <button className="format-action" type="button" onClick={() => this.format('outdent')}>&lt;</button>
+                    <button className="format-action" type="button" onClick={() => this.format('indent')}>&gt;</button>
                 </div>
             </div>
         );
--- src/file-zone/FileZone.js
+++ src/file-zone/FileZone.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import './FileZone.css';
 
 class FileZone extends Component {
+    constructor(props, context) {
+        super(props, context);
+        document.execCommand('styleWithCss', false, true);
+    }
+
     render() {
         return (
             <div id="file-zone">
